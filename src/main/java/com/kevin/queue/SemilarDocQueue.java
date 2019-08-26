47352847f26808f1b4fd7637d5e75dce60faf6fb
guyuefei <1530280162@qq.com>
2019-08-26 21:51:07 +0800

    修改队列参数类型

--- src/main/java/com/kevin/queue/SemilarDocQueue.java
+++ src/main/java/com/kevin/queue/SemilarDocQueue.java
@@ -1,17 +1,18 @@
-package com.blcultra.queue;
+package com.kevin.queue;
 
+import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class LineQueue {
+public class SemilarDocQueue {
 
     private static final LinkedBlockingQueue<Object> linequeue = new LinkedBlockingQueue();
 
-    public synchronized static  void push(String line) throws Exception{
+    public synchronized static  void push(List<String[]> line) throws Exception{
         linequeue.offer(line);
     }
     //出栈
-    public synchronized static String pop() throws Exception{
-       return (String)linequeue.poll();
+    public synchronized static List<String[]> pop() throws Exception{
+       return (List<String[]>)linequeue.poll();
     }
 
     public synchronized static  boolean isEmpty(){
